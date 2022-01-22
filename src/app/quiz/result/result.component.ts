3c3f91c8a9ce60bf4a5f099d3a7281991a6f478e
Johanna Kirsch <johkirsch@htwsaar.de>
2022-01-22 20:20:31 +0100

    questions quiz

--- src/app/quiz/result/result.component.ts
+++ src/app/quiz/result/result.component.ts
@@ -9,10 +9,30 @@ export class ResultComponent implements OnInit {
   @Input()
   scorePercentage!: number;
 
-  constructor() {
-  }
+  status!: ResultStatus;
+
 
   ngOnInit(): void {
+    if (this.scorePercentage > 75) {
+      this.status = new ResultStatus('Maître Jedi', 'assets/images/yoda.png', '');
+    } else if (this.scorePercentage > 50) {
+      this.status = new ResultStatus('Chevalier Jedi', 'assets/images/obiwan.png', '');
+    } else if (this.scorePercentage > 25) {
+      this.status = new ResultStatus('Padawan', 'assets/images/anakin.jpg', '')
+    } else if (this.scorePercentage > 0) {
+      this.status = new ResultStatus('Initié', 'assets/images/youngling.jpg', '')
+    } else {
+      this.status = new ResultStatus('Idiot', 'assets/images/jarjar.jpg', 'Votre capacité à parler ne vous rend pas intélligent')
+    }
   }
 
+}
+
+class ResultStatus {
+  constructor(
+    public rang: string,
+    public image: string,
+    public text: string
+  ) {
+  }
 }
